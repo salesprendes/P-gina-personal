61a2830f9b84276402424f9d3511c12ab276dbb3
Alvaro Prendes <salesprendes@gmail.com>
2018-10-03 13:21:06 +0200

    .
    .

--- index.php
+++ index.php
@@ -52,10 +52,10 @@
          <h1 class="section-big-title til hidden-xs hidden-sm" data-in-effect="fadeInUp" data-out-effect="fadeInDown">Información</h1>
          <div class="container">
             <div class="row">
-               <div class="col-sm-4 col-sm-push-4">
+               <div class="col-sm-4 col-sm-push-8">
                   <div class="section-holder texture">
                      <div class="img-holder">
-                     	<img src="https://instagram.fmad3-1.fna.fbcdn.net/vp/f9a06f9e3f530b6c0913bd51bcb15b86/5C41A376/t51.2885-19/s150x150/41820001_1881338575290669_8777588477735731200_n.jpg" class="rounded-circle" alt="Foto Personal">
+                     	<img src="https://instagram.fmad3-1.fna.fbcdn.net/vp/f9a06f9e3f530b6c0913bd51bcb15b86/5C41A376/t51.2885-19/s150x150/41820001_1881338575290669_8777588477735731200_n.jpg" class="img-responsive img-circle" alt="Foto Personal">
                      </div>
                      <div class="sep-mini"></div>
                      <h4> Nombre: Alvaro Prendes </h4>
@@ -286,6 +286,95 @@
                </div>
             </div>
          </div>
+      </section>
+
+      <section id="contact">
+        <h1 class="section-big-title til hidden-xs hidden-sm" data-in-effect="fadeInUp" data-out-effect="fadeInDown">contact</h1><!-- /. large title -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="section-holder texture">
+                        <div class="icon-holder i-size-big">
+                            <i class="ion-ios-wineglass-outline"></i>
+                        </div>
+                        <h3>Localización</h3>
+                        <h5>Dirección: Carrer de Pau Casals, 12, Salt, Girona</h5>
+                        <h5>Email: salesprendes@gmail.com</h5>
+                        <h5>Móvil: 616 729 303</h5>
+                        <a href="https://maps.google.com/maps?q=Carrer+de+Pau+Casals,+12,+17190+Salt,+Girona" class="mfp-iframe popup-it"><h4 class="map-open">Abrir mapa</h4></a>
+                        <div class="social-links">
+                           <a href="https://twitter.com/salesprendes"><i class="ion-social-twitter-outline i-size-small"></i></a>
+                           <a href="https://www.facebook.com/salesprendes"><i class="ion-social-facebook-outline i-size-small"></i></a>
+                           <a href="https://www.instagram.com/_.alvaro.prendes._/"><i class="ion-social-instagram-outline i-size-small"></i></a>
+                           <a href="https://github.com/salesprendes"><i class="ion-social-github-outline i-size-small"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-8">
+                    <div class="section-holder">
+                        <div class="icon-holder i-size-big">
+                            <i class="ion-ios-email-outline"></i>
+                        </div>
+                        <h3> Contacto </h3>
+                        <form class="form-horizontal" id="contactForm">
+                            <div class="form-group">
+
+                               <div class="col-sm-6">
+
+                                   <input type="text" class="form-control" id="inputName" placeholder="Tu Nombre" required>
+                                    <span class="form-control-feedback">
+                                        <i class="ion-ios-grid-view-outline"></i>
+                                    </span>
+
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <input type="email" class="form-control" id="inputEmail" placeholder="Tu correo" required>
+
+                                    <span class="form-control-feedback">
+                                        <i class="ion-ios-grid-view-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" id="inputPhone" placeholder="Telefono movil (Opcional)">
+
+                                    <span class="form-control-feedback">
+                                        <i class="ion-ios-grid-view-outline"></i>
+                                    </span>
+                                </div>
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" id="inputSubject" placeholder="Titulo" required>
+
+                                    <span class="form-control-feedback">
+                                        <i class="ion-ios-grid-view-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="form-group message">
+                                <div class="col-sm-12">
+                                    <textarea class="form-control" id="inputMessage" rows="4" placeholder="Mensaje" required></textarea>
+
+                                    <span class="form-control-feedback">
+                                        <i class="ion-ios-grid-view-outline"></i>
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-8">
+                                    <button type="submit" id="form-submit" class="btn btn-cl">Enviar Mensaje</button>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </form>
+                        <div id="msgSubmit" class="h3 text-center hidden"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
       </section>
 
       <script src="./assets/js/jquery-3.3.1.min.js" type="text/javascript"></script>
